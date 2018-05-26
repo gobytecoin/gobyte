e6ccedf0dab8994428d29db600f9d5a91df187b4
gobytecoin <dev@gobyte.network>
2018-05-26 17:07:18 +0800

    [Updated] SporkPubKey

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -172,7 +172,7 @@ public:
 
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 60*60; // fulfilled requests expire in 1 hour
-        strSporkPubKey = "042925f2904184e419f890076f94e24943b20e149b4ba703e90b03d974ed04325e2cb289941d220e161000ead5e1cefebe160993df6f3b23741fa5f6e4c6f7a123";
+        strSporkPubKey = "0436E3A43B74CB522C19CD0C2C8073152B60C227F1360C7D3249F995CC19466EE132D3843FCB3379F10C0EA2336112F1283DC78C04DF5C7BD39C8896627AC60D4C"
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
@@ -306,7 +306,7 @@ public:
 
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 5*60; // fulfilled requests expire in 5 minutes
-        strSporkPubKey = "042debfdefc73e408e3269318a5767d3ce3f996593e6a6596b10907e76381bd5f0862d0e418c0d6862e88c8db42bff39a0d37ea61cca41713007f1f098cd139f84";
+        strSporkPubKey = "0436E3A43B74CB522C19CD0C2C8073152B60C227F1360C7D3249F995CC19466EE132D3843FCB3379F10C0EA2336112F1283DC78C04DF5C7BD39C8896627AC60D4C";
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
