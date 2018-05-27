dae45647421e5aaadf4c857359aeb373007e6c45
gobytecoin <dev@gobyte.network>
2018-05-27 18:31:36 +0800

    Registered coin types for BIP-0044 https://github.com/satoshilabs/slips/blob/master/slip-0044.md

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -159,8 +159,8 @@ public:
         // GoByte BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // GoByte BIP44 coin type is '5'
-        nExtCoinType = 5;
+        // GoByte BIP44 coin type is '176'
+        nExtCoinType = 176;
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
