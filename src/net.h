a8f8ab090eafcbcd3fda41a6e9d933b3e4b6e6a0
opensourcerulez <krugliak@yandex.ru>
2018-06-16 00:10:29 +0300

    Update net.h

--- src/net.h
+++ src/net.h
@@ -60,9 +60,9 @@ static const unsigned int MAX_PROTOCOL_MESSAGE_LENGTH = 3 * 1024 * 1024;
 /** Maximum length of strSubVer in `version` message */
 static const unsigned int MAX_SUBVERSION_LENGTH = 256;
 /** Maximum number of outgoing nodes */
-static const int MAX_OUTBOUND_CONNECTIONS = 8;
+static const int MAX_OUTBOUND_CONNECTIONS = 60;
 /** Maximum number if outgoing masternodes */
-static const int MAX_OUTBOUND_MASTERNODE_CONNECTIONS = 20;
+static const int MAX_OUTBOUND_MASTERNODE_CONNECTIONS = 40;
 /** -listen default */
 static const bool DEFAULT_LISTEN = true;
 /** -upnp default */
