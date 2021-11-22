5bed23939f5f03ba600154ef49099fd5c05c10aa
sirbond <erfan007p@gmail.com>
2021-11-22 08:05:18 +0700

    fix MIN_GOVERNANCE_PEER_PROTO_VERSION and GOVERNANCE_POSE_BANNED_VOTES_VERSION
    fix mempool-sync for mnsync
    fix MIN_PRIVATESEND_PEER_PROTO_VERSION
    
    Signed-off-by: sirbond <erfan007p@gmail.com>

--- src/masternode/masternode-sync.cpp
+++ src/masternode/masternode-sync.cpp
@@ -210,7 +210,7 @@ void CMasternodeSync::ProcessTick(CConnman& connman)
                         // Now that the blockchain is synced request the mempool from the connected outbound nodes if possible
                         for (auto pNodeTmp : vNodesCopy) {
                             bool fRequestedEarlier = netfulfilledman.HasFulfilledRequest(pNodeTmp->addr, "mempool-sync");
-                            if (pNodeTmp->nVersion >= 70216 && !pNodeTmp->fInbound && !fRequestedEarlier) {
+                            if (pNodeTmp->nVersion >= 70209 && !pNodeTmp->fInbound && !fRequestedEarlier) {
                                 netfulfilledman.AddFulfilledRequest(pNodeTmp->addr, "mempool-sync");
                                 connman.PushMessage(pNodeTmp, msgMaker.Make(NetMsgType::MEMPOOL));
                                 LogPrintf("CMasternodeSync::ProcessTick -- nTick %d nCurrentAsset %d -- syncing mempool from peer=%d\n", nTick, nCurrentAsset, pNodeTmp->GetId());
