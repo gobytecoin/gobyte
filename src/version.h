155173c9a589cffe45e49a7721ec0120fa960427
sirbond <erfan007p@gmail.com>
2021-11-22 08:06:15 +0700

    bump proto version to 70210
    fix PROTOCOL_VERSION to proto version
    
    Signed-off-by: sirbond <erfan007p@gmail.com>

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70209;
+static const int PROTOCOL_VERSION = 70210;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -23,7 +23,7 @@ static const int GETHEADERS_VERSION = 70077;
 static const int MIN_PEER_PROTO_VERSION = 70209;
 
 //! minimum proto version of masternode to accept in DKGs
-static const int MIN_MASTERNODE_PROTO_VERSION = PROTOCOL_VERSION;
+static const int MIN_MASTERNODE_PROTO_VERSION = 70210;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -42,19 +42,19 @@ static const int SENDHEADERS_VERSION = 70201;
 static const int DIP0001_PROTOCOL_VERSION = 70208;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = PROTOCOL_VERSION;
+static const int SHORT_IDS_BLOCKS_VERSION = 70209;
 
 //! introduction of DIP3/deterministic masternodes
-static const int DMN_PROTO_VERSION = PROTOCOL_VERSION;
+static const int DMN_PROTO_VERSION = 70209;
 
 //! introduction of LLMQs
-static const int LLMQS_PROTO_VERSION = PROTOCOL_VERSION;
+static const int LLMQS_PROTO_VERSION = 70209;
 
 //! introduction of SENDDSQUEUE
 //! TODO we can remove this in 0.15.0.0
-static const int SENDDSQUEUE_PROTO_VERSION = PROTOCOL_VERSION;
+static const int SENDDSQUEUE_PROTO_VERSION = 70209;
 
 //! protocol version is included in MNAUTH starting with this version
-static const int MNAUTH_NODE_VER_VERSION = PROTOCOL_VERSION;
+static const int MNAUTH_NODE_VER_VERSION = 70210;
 
 #endif // BITCOIN_VERSION_H
