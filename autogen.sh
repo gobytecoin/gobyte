8a6c9e16e361d5fd11ba4f2b9afbdbcf17b83f33
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-04-02 21:17:15 +0300

    lint: carefuly fix shellcheck warnings in autogen.sh
    Fixes unquoted variable expansion in conditional check and adds shellcheck disable directive for expected behavior.

--- autogen.sh
+++ autogen.sh
@@ -5,9 +5,10 @@
 
 export LC_ALL=C
 set -e
+# shellcheck disable=SC2086
 srcdir="$(dirname $0)"
 cd "$srcdir"
-if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
+if [ -z "${LIBTOOLIZE}" ] && GLIBTOOLIZE="$(which glibtoolize 2>/dev/null)"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
