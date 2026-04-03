73e1d95c1fcba81752771e0e62e828b159943f9e
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-04-04 01:24:06 +0300

    build(deps): fix GMP configuration for x86_64 Darwin
    - Remove `--enable-fat` flag and use `=` instead of `+=` for `config_opts`
    - Add explicit `ABI=64` setting for x86_64 Darwin to ensure correct ABI detection
    - Update `config_cmds` to prepend ABI setting when building for x86_64 Darwin

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -5,7 +5,8 @@ $(package)_file_name=gmp-$($(package)_version).tar.bz2
 $(package)_sha256_hash=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
 
 define $(package)_set_vars
-$(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
+$(package)_config_opts=--enable-cxx --with-pic --disable-shared
+$(package)_config_opts_x86_64_darwin=ABI=64
 $(package)_cflags_armv7l_linux+=-march=armv7-a
 # When cross-compiling for Darwin on Linux, GMP's ABI detection probe uses
 # only $CC $CFLAGS (not $LDFLAGS).  Without these flags the probe invokes
@@ -20,7 +21,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $(if $(filter x86_64%darwin,$(host)),ABI=64 )$($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
